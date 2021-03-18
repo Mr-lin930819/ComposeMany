688a428ada30deadc0fce43a20a51acbb73a2bcc
linwei <linwei@landicorp.com>
2021-03-19 01:09:28 +0800

    库版本更新

--- app/src/main/java/com/mrlin/composemany/MainActivity.kt
+++ app/src/main/java/com/mrlin/composemany/MainActivity.kt
@@ -3,11 +3,23 @@ package com.mrlin.composemany
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mrlin.composemany.ui.theme.ComposeManyTheme
 
 class MainActivity : AppCompatActivity() {
@@ -24,9 +36,35 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Scaffold(topBar = {
+        TopAppBar(title = {
+            Text(text = "Compose Many")
+        }, navigationIcon = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = ""
+                )
+            }
+        })
+    }) {
+        LazyVerticalGrid(cells = GridCells.Fixed(count = 4), content = {
+            items(40, itemContent = {
+                Box(modifier = Modifier.border(width = 0.5.dp, color = Color.Gray)) {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                            contentDescription = ""
+                        )
+                        Text(text = "Hello ${it + 1}!")
+                    }
+                }
+            })
+        })
+    }
 }
 
 @Preview(showBackground = true)
