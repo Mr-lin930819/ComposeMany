a3f36036c63bb39e4228a5aac99953494585df23
林炜 <linwei@landicorp.com>
2021-04-09 18:42:46 +0800

    调整主界面

--- app/src/main/java/com/mrlin/composemany/MainActivity.kt
+++ app/src/main/java/com/mrlin/composemany/MainActivity.kt
@@ -8,12 +8,16 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -21,6 +25,10 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mrlin.composemany.ui.theme.ComposeManyTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,19 +46,36 @@ class MainActivity : AppCompatActivity() {
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun Greeting(name: String) {
+fun Greeting(name: String, scaffoldState: ScaffoldState = rememberScaffoldState()) {
+    val composableScope = rememberCoroutineScope()
+    val dateTime = remember { mutableStateOf("") }
+    composableScope.launch {
+        while (true) {
+            delay(1000)
+            dateTime.value = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.CHINA).format(Date())
+        }
+    }
     Scaffold(topBar = {
         TopAppBar(title = {
-            Text(text = "Compose Many")
+            Row {
+                Text(text = "百斯贝测试\t")
+                Text(text = dateTime.value)
+            }
         }, navigationIcon = {
-            IconButton(onClick = { /*TODO*/ }) {
+            IconButton(onClick = {
+                composableScope.launch {
+                    scaffoldState.drawerState.open()
+                }
+            }) {
                 Icon(
-                    imageVector = Icons.Filled.ArrowBack,
+                    imageVector = Icons.Filled.Menu,
                     contentDescription = ""
                 )
             }
         })
-    }) {
+    }, drawerContent = {
+        Text(text = "Hello!")
+    }, scaffoldState = scaffoldState) {
         LazyVerticalGrid(cells = GridCells.Fixed(count = 4), content = {
             items(40, itemContent = {
                 Box(modifier = Modifier.border(width = 0.5.dp, color = Color.Gray)) {
