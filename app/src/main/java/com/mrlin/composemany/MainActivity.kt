ecfc6e5422d1cc09203da300827eb2ee1a728f12
林炜 <linwei@landicorp.com>
2021-05-28 18:37:15 +0800

    调整主界面

--- app/src/main/java/com/mrlin/composemany/MainActivity.kt
+++ app/src/main/java/com/mrlin/composemany/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.GridCells
@@ -18,7 +19,11 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.colorspace.ColorModel
+import androidx.compose.ui.graphics.colorspace.ColorSpace
+import androidx.compose.ui.graphics.colorspace.ColorSpaces
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -52,7 +57,9 @@ fun Greeting(
     Scaffold(topBar = {
         TopAppBar(title = {
             Row(
-                Modifier.fillMaxWidth().padding(all = 8.dp),
+                Modifier
+                    .fillMaxWidth()
+                    .padding(all = 8.dp),
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
@@ -74,19 +81,53 @@ fun Greeting(
     }, drawerContent = {
         Text(text = "Hello!")
     }, scaffoldState = scaffoldState) {
-        LazyVerticalGrid(cells = GridCells.Fixed(count = 4), content = {
-            items(40, itemContent = {
-                Box(modifier = Modifier.border(width = 0.5.dp, color = Color.Gray)) {
-                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                        Image(
-                            painter = painterResource(id = R.drawable.ic_launcher_foreground),
-                            contentDescription = ""
-                        )
-                        Text(text = "Hello ${it + 1}!")
-                    }
+        Column {
+            Box(
+                Modifier
+                    .height(80.dp)
+                    .fillMaxWidth()
+                    .background(
+                        color = Color.Blue
+                            .convert(ColorSpaces.CieLab)
+                            .copy(green = -60f)
+                    )
+            )
+            Card(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(all = 8.dp)
+            ) {
+                Row {
+                    HeadTip(Modifier.weight(1.0f), title = "稳健理财", content = "近一年3.53%")
+                    Box(
+                        Modifier
+                            .wrapContentHeight()
+                            .width(1.dp)
+                            .background(Color.Gray.copy(alpha = 0.2f))
+                    )
+                    HeadTip(Modifier.weight(1.0f), title = "进阶理财", content = "追求更高收益")
                 }
-            })
-        })
+            }
+        }
+    }
+}
+
+@Composable
+fun HeadTip(modifier: Modifier = Modifier, title: String = "", content: String = "") {
+    Box(modifier = modifier.padding(8.dp)) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            Column {
+                Text(text = title, style = MaterialTheme.typography.h6)
+                Text(text = content, style = MaterialTheme.typography.caption)
+            }
+            Icon(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = ""
+            )
+        }
     }
 }
 
