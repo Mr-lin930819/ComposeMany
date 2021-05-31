b66be021107152f91421f75c49e0a9365ca08d29
林炜 <linwei@landicorp.com>
2021-05-31 16:29:46 +0800

    模仿支付宝理财页实现主页面

--- app/src/main/java/com/mrlin/composemany/MainActivity.kt
+++ app/src/main/java/com/mrlin/composemany/MainActivity.kt
@@ -2,14 +2,14 @@ package com.mrlin.composemany
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.GridCells
-import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
@@ -19,41 +19,41 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.colorspace.ColorModel
-import androidx.compose.ui.graphics.colorspace.ColorSpace
 import androidx.compose.ui.graphics.colorspace.ColorSpaces
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.mrlin.composemany.ui.theme.ComposeManyTheme
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeManyTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    viewModel.runTimer()
+                    Greeting("Android", viewModel.time)
                 }
             }
         }
     }
 }
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun Greeting(
-    name: String, scaffoldState: ScaffoldState = rememberScaffoldState(),
-    viewModel: MainViewModel = viewModel()
+    name: String,
+    dataTimeData: LiveData<String>,
+    scaffoldState: ScaffoldState = rememberScaffoldState(),
 ) {
     val composableScope = rememberCoroutineScope()
-    val dateTime: String by viewModel.time.observeAsState("")
-    viewModel.runTimer()
+    val dateTime: String by dataTimeData.observeAsState("")
     Scaffold(topBar = {
         TopAppBar(title = {
             Row(
@@ -80,8 +80,8 @@ fun Greeting(
         })
     }, drawerContent = {
         Text(text = "Hello!")
-    }, scaffoldState = scaffoldState) {
-        Column {
+    }, scaffoldState = scaffoldState, backgroundColor = Color.LightGray.copy(alpha = 0.3f)) {
+        Column(Modifier.verticalScroll(rememberScrollState())) {
             Box(
                 Modifier
                     .height(80.dp)
@@ -94,19 +94,28 @@ fun Greeting(
             )
             Card(
                 Modifier
-                    .fillMaxWidth()
                     .padding(all = 8.dp)
+                    .fillMaxWidth(),
+                shape = MaterialTheme.shapes.medium.copy(all = CornerSize(10.dp))
             ) {
-                Row {
+                Row(verticalAlignment = Alignment.CenterVertically) {
                     HeadTip(Modifier.weight(1.0f), title = "稳健理财", content = "近一年3.53%")
                     Box(
                         Modifier
-                            .wrapContentHeight()
+                            .height(48.dp)
                             .width(1.dp)
                             .background(Color.Gray.copy(alpha = 0.2f))
                     )
                     HeadTip(Modifier.weight(1.0f), title = "进阶理财", content = "追求更高收益")
                 }
+            }
+            Card(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                shape = MaterialTheme.shapes.medium.copy(all = CornerSize(10.dp))
+            ) {
+                DetailTip()
             }
         }
     }
@@ -123,10 +132,145 @@ fun HeadTip(modifier: Modifier = Modifier, title: String = "", content: String =
                 Text(text = title, style = MaterialTheme.typography.h6)
                 Text(text = content, style = MaterialTheme.typography.caption)
             }
-            Icon(
-                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            Image(
+                modifier = Modifier.width(48.dp).height(48.dp),
+                painter = painterResource(id = R.drawable.discuss),
                 contentDescription = ""
             )
+        }
+    }
+}
+
+@Composable
+fun DetailTip() {
+    Box(
+        Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Column {
+            Row {
+                IndexBox(Modifier.weight(1.0f), "上证指数", 3593.61, -0.20)
+                IndexBox(Modifier.weight(1.0f), "深证成指", 14905.05, 0.35)
+                IndexBox(Modifier.weight(1.0f), "创业板指", 3285.36, 1.64)
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Row {
+                Box(Modifier.weight(1.0f)) {
+                    HotBox(title = "今日热点", stamp = "快讯 14-12") {
+                        HotFundView()
+                    }
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                Box(Modifier.weight(1.0f)) {
+                    Column {
+                        HotBox(title = "基金自选") {
+                            Column {
+                                Text(text = "嘉实物流产业股票")
+                                Text(text = "+23.43%")
+                                Text(text = "近6月", style = MaterialTheme.typography.caption)
+                            }
+                        }
+                        Spacer(modifier = Modifier.height(8.dp))
+                        HotBox(title = "直播中") {
+                            Column {
+                                Text(text = "正在讲解重磅新发基金")
+                                Text(text = "13.3万观看", style = MaterialTheme.typography.caption)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+/**
+ * 热门基金
+ */
+@Composable
+fun HotFundView() {
+    Column {
+        Row(Modifier.height(50.dp)) {
+            Box(
+                modifier = Modifier
+                    .weight(1.0f)
+                    .fillMaxHeight()
+                    .background(Color.Red.copy(alpha = 0.3f))
+            )
+            Spacer(modifier = Modifier.width(1.dp))
+            Box(
+                modifier = Modifier
+                    .weight(1.0f)
+                    .fillMaxHeight()
+                    .background(Color.Red.copy(alpha = 0.3f))
+            )
+        }
+        Spacer(modifier = Modifier.height(1.dp))
+        Row(Modifier.height(50.dp)) {
+            Box(
+                modifier = Modifier
+                    .weight(1.0f)
+                    .fillMaxHeight()
+                    .background(Color.Green.copy(alpha = 0.3f))
+            )
+            Spacer(modifier = Modifier.width(1.dp))
+            Box(
+                modifier = Modifier
+                    .weight(1.0f)
+                    .fillMaxHeight()
+                    .background(Color.Green.copy(alpha = 0.3f))
+            )
+        }
+        Text(text = "独家电报|律所主任实名举报正川股份")
+    }
+}
+
+@Composable
+fun IndexBox(modifier: Modifier, indexName: String, indexValue: Double, indexRate: Double) {
+    Box(modifier = modifier) {
+        val color = if (indexRate < 0) Color.Green else Color.Red
+        Column {
+            Text(text = indexName)
+            Text(
+                text = indexValue.toBigDecimal().toPlainString(),
+                style = LocalTextStyle.current.copy(color = color)
+            )
+            Text(
+                text = "${indexRate.toBigDecimal().toPlainString()}%",
+                style = MaterialTheme.typography.caption.copy(
+                    color = color
+                )
+            )
+        }
+    }
+}
+
+@Composable
+fun HotBox(title: String, stamp: String = "", content: @Composable (BoxScope.() -> Unit)) {
+    Card(Modifier.fillMaxWidth(), backgroundColor = Color.LightGray.copy(alpha = 0.2f), elevation = 0.dp) {
+        Column(Modifier.padding(8.dp)) {
+            Text(text = title, fontWeight = FontWeight.Bold)
+            Box(Modifier.padding(vertical = 8.dp), content = content)
+            if (stamp.isNotEmpty()) {
+                Text(text = stamp, style = MaterialTheme.typography.caption)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HotPreview() {
+    HotBox(title = "今日热点", stamp = "快讯 14-12") {
+        Column {
+            Box(
+                modifier = Modifier
+                    .width(100.dp)
+                    .height(100.dp)
+                    .background(Color.Red.copy(alpha = 0.3f))
+            )
+            Text(text = "独家电报|律所主任实名举报正川股份")
         }
     }
 }
@@ -135,6 +279,6 @@ fun HeadTip(modifier: Modifier = Modifier, title: String = "", content: String =
 @Composable
 fun DefaultPreview() {
     ComposeManyTheme {
-        Greeting("Android")
+        Greeting("Android", MutableLiveData("2020-10-10 10:00:00"))
     }
 }
